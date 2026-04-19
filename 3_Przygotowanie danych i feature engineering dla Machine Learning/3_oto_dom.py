60ebb590fa995d44b4fe579f96d03a118648523c
student-wroclaw <student-wroclaw@EDU-WSB.local>
2026-04-19 10:28:34 +0200

    '1'

--- 3_Przygotowanie danych i feature engineering dla Machine Learning/3_oto_dom.py
+++ 3_Przygotowanie danych i feature engineering dla Machine Learning/3_oto_dom.py
@@ -14,3 +14,10 @@ plt.show()
 
 plt.scatter(df.powierzchnia, df.cena)
 plt.show()
+
+q1 = df.describe().loc["25%", 'cena']
+q3 = df.describe().loc["75%", 'cena']
+
+df1 = df[ (df.cena >= q1) & (df.cena <= q3)  ]
+sns.histplot(df1.cena)
+plt.show()
